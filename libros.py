3ba568f2238e61f61ddc4e8a1dc626729b28855e
Joel Ramirez Garay <113269346+EnriqueR20@users.noreply.github.com>
2022-11-06 10:27:29 -0500

    se agrego clase
    Mi clase libro

--- libros.py
+++ libros.py
@@ -1,1 +1,10 @@
+class libro():
+    def __init__(self,id,título,género,ISBN,editorial,autor):
+        self.id=id
+        self.título = título
+        self.género = género
+        self.ISBN = ISBN
+        self.editorial = editorial
+        self.autor = autor
+
 

1c98432ec7030337c29688660f4b2aad2cddee85
Yoselinrudas <113314189+Yoselinrudas@users.noreply.github.com>
2022-11-06 17:30:19 -0600

    import Book
    llamo al archivo book dentro de este .py

--- LIBRO.py
+++ LIBRO.py
@@ -1,1 +1,22 @@
 
+from typing import List
+
+from Book import *
+
+
+class Biblioteca:
+
+
+    def __init__(self):
+        self._books = []
+        self._file_name = None
+
+    @property
+    def file_name(self):
+        return self._file_name
+
+    @file_name.setter
+    def file_name(self, value):
+        self._file_name = value
+
+
